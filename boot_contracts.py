d26ccf9b82478ea835c38b5d10da36cacf4901b3
nolash <dev@holbrook.no>
2020-02-25 18:55:23 +0100

    WIP creating two sided convert narrative contract txs

--- boot_contracts.py
+++ boot_contracts.py
@@ -26,8 +26,54 @@ s = Session(
 logg.debug("session: %s %s %s", s.root, s.rpc, s.w3.clientVersion)
 
 
-# deploy ethertoken contract
-c = ContractTransaction(s.w3)
+# deploy two ethertoken contracts
+# these hold the reserves of each side of cic conversion
+
+# contract addresses for the reserve tokens
+addr_reserve_one = ''
+addr_reserve_two = ''
+addr_smart_one = ''
+addr_smart_two = ''
+
+c = ContractTransaction(s.w3, s.w3.eth.accounts[0])
 c.fromJsonFile(s.root + "/data/" + 'EtherToken.json')
 c.deploy(s.w3.eth.accounts[0])
-logg.debug("deploy ethertoken rcpt %s", c.rcpt)
+addr_reserve_one = c.rcpt['contractAddress']
+
+c = ContractTransaction(s.w3, s.w3.eth.accounts[1])
+c.fromJsonFile(s.root + "/data/" + 'EtherToken.json')
+c.deploy(s.w3.eth.accounts[1])
+addr_reserve_two = c.rcpt['contractAddress']
+
+
+# deposit eth in the reserve tokens
+c = ContractTransaction(s.w3, s.w3.eth.accounts[0])
+c.fromJsonFile(s.root + "/data/" + 'EtherToken.json')
+c.callValue(addr_reserve_one, 1000000, 'deposit')
+
+c = ContractTransaction(s.w3, s.w3.eth.accounts[0])
+c.fromJsonFile(s.root + "/data/" + 'EtherToken.json')
+c.callValue(addr_reserve_two, 1000000, 'deposit')
+
+
+# deploy two smarttoken contracts
+c = ContractTransaction(s.w3, s.w3.eth.accounts[0])
+c.fromJsonFile(s.root + '/data/' + 'SmartToken.json')
+c.deploy(s.w3.eth.accounts[0], 'SmartOne', 'SM1', 3)
+addr_smart_one = c.rcpt['contractAddress']
+
+c = ContractTransaction(s.w3, s.w3.eth.accounts[1])
+c.fromJsonFile(s.root + '/data/' + 'SmartToken.json')
+c.deploy(s.w3.eth.accounts[1], 'SmartOne', 'SM2', 3)
+addr_smart_two = c.rcpt['contractAddress']
+
+
+# issue smart tokens
+c = ContractTransaction(s.w3, s.w3.eth.accounts[0])
+c.fromJsonFile(s.root + '/data/' + 'SmartToken.json', addr_smart_one)
+c.call(s.w3.eth.accounts[0], 'issue', s.w3.eth.accounts[0], 40000)
+
+c = ContractTransaction(s.w3, s.w3.eth.accounts[1])
+c.fromJsonFile(s.root + '/data/' + 'SmartToken.json', addr_smart_two)
+c.call(s.w3.eth.accounts[1], 'issue', s.w3.eth.accounts[1], 40000)
+
--- contract.py
+++ contract.py
@@ -1,5 +1,8 @@
 import json
 import os
+import logging
+
+logg = logging.getLogger('contract')
 
 class ContractTransaction:
 
@@ -8,11 +11,11 @@ class ContractTransaction:
     Args:
         w3l: If speificed sets static web3 object for all contract transactions
     """
-    w3 = None
-    def __init__(self, w3l):
+    def __init__(self, w3l, addr):
         self.contract = None
         self.hash = None
         self.rcpt = None
+        self.addr = addr
         if w3l != None:
             self.w3 = w3l
 
@@ -27,12 +30,13 @@ class ContractTransaction:
     todo:
         check if json format from truffle is same as remix etc
     """
-    def fromJsonFile(self, js_file):
+    def fromJsonFile(self, js_file, address=None):
         fd = os.open(js_file, os.O_RDONLY)
         data = os.read(fd, 1024000)
         os.close(fd)
         contract_json = json.loads(data)
-        self.contract = self.w3.eth.contract(bytecode=contract_json['bytecode'], abi=contract_json['abi'])
+        self.contract = self.w3.eth.contract(address=address, bytecode=contract_json['bytecode'], abi=contract_json['abi'])
+
 
     """Call constructor for contract
 
@@ -40,19 +44,56 @@ class ContractTransaction:
         account:
             web3 account object
     """
-    def deploy(self, account):
-        self.hash = self.contract.constructor().transact({'from': account})
-        self.rcpt = self.w3.eth.waitForTransactionReceipt(self.hash)
+    def deploy(self, account, *args):
+        hsh = ''
+        if len(args) == 0:
+            hsh = self._deployNoArgs(account)
+        if len(args) == 3:
+            hsh = self._deployThreeArgs(account, args[0], args[1], args[2])
+        self.rcpt = self.w3.eth.waitForTransactionReceipt(hsh)
+        self.hash = hsh
+
+    def _deployNoArgs(self, account):
+       return self.contract.constructor().transact({'from': account})
+
+    def _deployThreeArgs(self, account, one, two, three):
+       return self.contract.constructor(one, two, three).transact({'from': account})
+    
 
     """Call function on contract
 
     Args:
+        addr:
+            address of contract
+        value: 
+            eth value to send to contract (account must have enough balance)
         functionName:
             function name to call
         args:
-            array of args to function
+            array of args to pass to contract function
     """        
-    def call(self, functionName):
-        pass
+    def callValue(self, addr, value, functionName, *args):
+        f = self.contract.get_function_by_name(functionName)
+        if len(args) == 1:
+            return self._callValueOneArg(f, args[0])
+
+
+    def _callValueOneArg(self, f, arg):
+        return f(args).transact({
+            'from': self.addr,
+            'value': value,
+        })
+
+
+    def call(self, addr, functionName, *args):
+        f = self.contract.get_function_by_name(functionName)
+        if len(args) == 2:
+            return self._callTwoArg(f, addr, args[0], args[1])
+
+
+    def _callTwoArg(self, f, addr, one, two):
+        return f(one, two).transact({
+            'from': self.addr,
+        })
 
 
