fd7ad729a585debf81de9c998d008b590456ff5d
nolash <dev@holbrook.no>
2020-02-26 18:42:51 +0100

    Make successful single convert with smarttoken as from-token

--- contract.py
+++ contract.py
@@ -1,9 +1,9 @@
 import json
 import os
 import logging
-import time
 
 logg = logging.getLogger('contract')
+GAS_DONTCARE = 8000000
 
 class ContractTransaction:
 
@@ -47,18 +47,18 @@ class ContractTransaction:
     """
     def deploy(self, account, *args):
         hsh = ''
-        if len(args) == 0:
-            hsh = self._deployNoArgs(account)
         if len(args) == 1:
             hsh = self._deployOneArgs(account, args[0])
-        if len(args) == 2:
+        elif len(args) == 2:
             hsh = self._deployTwoArgs(account, args[0], args[1])
-        if len(args) == 3:
+        elif len(args) == 3:
             hsh = self._deployThreeArgs(account, args[0], args[1], args[2])
-        if len(args) == 4:
+        elif len(args) == 4:
             hsh = self._deployFourArgs(account, args[0], args[1], args[2], args[3])
-        if len(args) == 5:
+        elif len(args) == 5:
             hsh = self._deployFiveArgs(account, args[0], args[1], args[2], args[3], args[4])
+        else:
+            hsh = self._deployNoArgs(account)
         if hsh == '':
             raise Exception('hash missing ' + str(self.contract))
         logg.debug("deploy hash %s", hsh)
@@ -129,25 +129,70 @@ class ContractTransaction:
             array of args to pass to contract function
     """        
     def call(self, addr, functionName, *args):
+        hsh = ''
         f = self.contract.get_function_by_name(functionName)
         if len(args) == 1:
-            return self._callOneArgs(f, addr, args[0])
-        if len(args) == 2:
-            return self._callTwoArgs(f, addr, args[0], args[1])
+            hsh = self._callOneArgs(f, addr, args[0])
+        elif len(args) == 2:
+            hsh = self._callTwoArgs(f, addr, args[0], args[1])
+        elif len(args) == 3:
+            hsh = self._callThreeArgs(f, addr, args[0], args[1], args[2])
+        elif len(args) == 4:
+            hsh = self._callFourArgs(f, addr, args[0], args[1], args[2], args[3])
         else:
-            return self._callNoArgs(f, addr)
+            hsh = self._callNoArgs(f, addr)
+        self.rcpt = self.w3.eth.waitForTransactionReceipt(hsh)
+        self.hash = hsh.hex()
+
 
     def _callNoArgs(self, f, addr):
         return f().transact({
             'from': self.addr,
+            'gas': GAS_DONTCARE,
+            'gasPrice': 10,
         })
  
     def _callOneArgs(self, f, addr, one):
         return f(one).transact({
             'from': self.addr,
+            'gas': GAS_DONTCARE,
+            'gasPrice': 10,
         })
 
     def _callTwoArgs(self, f, addr, one, two):
         return f(one, two).transact({
             'from': self.addr,
+            'gas': GAS_DONTCARE,
+            'gasPrice': 10,
         })
+
+    def _callThreeArgs(self, f, addr, one, two, three):
+        return f(one, two, three).transact({
+            'from': self.addr,
+            'gas': GAS_DONTCARE,
+            'gasPrice': 10,
+        })
+
+
+    def _callFourArgs(self, f, addr, one, two, three, four):
+        return f(one, two, three, four).transact({
+            'from': self.addr,
+            'gas': GAS_DONTCARE,
+            'gasPrice': 10,
+        })
+
+
+    def read(self, addr, functionName, *args):
+        f = self.contract.get_function_by_name(functionName)
+        if len(args) == 1:
+            return f(args[0]).call()
+        elif len(args) == 2:
+            return f(args[0], args[1]).call()
+        elif len(args) == 3:
+            return f(args[0], args[1], args[2]).call()
+        elif len(args) == 4:
+            return f(args[0], args[1], args[2], args[3]).call()
+        else:
+            return f().call()
+
+
